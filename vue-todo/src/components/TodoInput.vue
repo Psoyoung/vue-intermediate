fe072659fca7451ef61ffcf7604fb4848ebadf8f
soyoung <6545742@naver.com>
2023-10-20 17:46:49 +0900

    componets 파일 생성

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="inputBox">
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
+    <span class="addContainer">
+      <i class="fa-solid fa-plus"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function(){
+      //console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
